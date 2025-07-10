28f08d7e6f96040db946a0853e7ec8dd0f8a5a82
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-07-10 12:29:19 +0200

    Creating the Basic Arithmetic Exercise for learing python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+#To practice basic arithmetic operations in Python by performing predefined calculations.
+
+number1 = 10
+number2 = 5
+
+addition = float(number1 + number2)
+subtraction = float(number1 - number2)
+multiplication = float(number1*number2)
+
+print ("Addition of", number1, "and", number2, "is", addition)
+print ("Subtration of", number1, "and", number2, "is", subtraction)
+print ("Multiplication", number1, "and", number2, "is", multiplication)
