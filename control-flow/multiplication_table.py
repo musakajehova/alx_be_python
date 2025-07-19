c89b66ff74bef1089a88338a765c0bc1dbaae29d
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-07-19 08:15:12 +0200

    This cleans up the code for multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,5 +3,4 @@
 number = int(input("Enter a number to see its multiplication table:"))
 
 for multiple in range(1, 11):
-    #print(number, "*", multiple, "=", multiple*number)
     print(f"{number} * {multiple} = {multiple * number}")
