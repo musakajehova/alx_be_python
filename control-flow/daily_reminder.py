a875c68e937e3b46ddd018e7a6199bd93a522d7a
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-07-19 12:30:55 +0200

    Modify time variable to time_bound in daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,21 +2,21 @@
 
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
-time = input("Is it time-bound? (yes/no): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
     case "high":
-        if time == "yes":
+        if time_bound == "yes":
             print("'",task, "'" , "is a high priority task that requires immediate attention today!")
         else:
             print("'",task, "'" , "is a high priority task that requires your attention as soon as you are avaliable")
     case "meduim":
-        if time == "yes":
+        if time_bound == "yes":
             print("'",task, "'" , "is a medium priority task that requires your attention today!")
         else:
             print("'",task, "'" , "is a medium priority task that is required to be completed sometime during the day")
     case "low":
-        if time == "yes":
+        if time_bound == "yes":
             print("'",task, "'" , "is a low priority task that requires your timely attention after tending to pressing matters!")
         else:
             print("'",task, "'" , "is a low priority task. Consider completing it when you have free time.")
