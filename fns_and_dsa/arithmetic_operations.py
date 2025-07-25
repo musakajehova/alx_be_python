887578f58ed9e1ba0b421360450bb1c602a55eb1
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-07-25 20:46:21 +0200

    This creates all thats is needed for task 1

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,21 @@
+# 1. Simple Calculator with Match Case
+
+
+def perform_operation (num1: float, num2: float, operation: str) -> float:
+    '''This performs arithmatic'''
+    
+
+    match operation:
+        case "add":
+            result = num1 + num2
+        case "subtract":
+            result = num1 - num2
+        case "multiply":
+            result = num1 * num2
+        case "divide":
+            if num2 == 0:
+                print("Cannot divide by zero.")
+            else:
+                result = num1/num2
+
+    return result
