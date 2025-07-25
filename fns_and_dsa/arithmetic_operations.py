4218fcfd51421c0c0f648bb70c9e8e0eb49c8afe
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-07-25 21:05:26 +0200

    no 3 correction

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,7 @@
 # 1. Simple Calculator with Match Case
 
 
-def perform_operation (num1, num2, operation):
+def perform_operation(num1: float, num2: float, operation: str) -> float:
     '''This performs arithmatic'''
     
 
