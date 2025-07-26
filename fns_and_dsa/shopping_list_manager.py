876132a95c9372496816993817ddf82820996b0b
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-07-26 07:37:18 +0200

    Task 1 shopping list, correction 1

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,7 +14,7 @@ def main():
         if choice == '1':
             # Prompt for and add an item
             print("\n")
-            listadd = input("What would you like to add to the list: ")
+            listadd = input("Enter the item to add: ")
             shopping_list.append(listadd)
         elif choice == '2':
             # Prompt for and remove an item
