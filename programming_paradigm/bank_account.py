a8b3a1d6a49b1c226370046b8e2aeebcc8b6797d
Musakajehova Manyathi <musakajehovalindo@gmail.com>
2025-08-02 16:31:37 +0200

    Task 0 attempt 1

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+#bank_account.py
+
+class BankAccount :
+
+    def __init__(self, initial_balance=0.0):
+        self.account_balance = float(initial_balance)
+
+
+    def deposit(self, amount):
+        '''This adds to the balance'''
+        self.account_balance += amount
+        
+
+    def withdraw(self, amount):
+        '''This subtracts the balance'''
+        if (self.account_balance - amount) < 0:
+            return False
+        else:    
+            self.account_balance -= amount
+            return True
+
+    def display_balance(self):
+        '''This displays the current balance'''
+        print(f"The balance is: {self.account_balance}")
